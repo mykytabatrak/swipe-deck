b058eb61cfd8b86c9be8150797ae7499f75f2aa2
Mykyta Batrak <mykbatrak@gmail.com>
2026-01-02 21:52:33 -0600

    Implemented swipe deck

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,8 @@ import "./App.css";
 export function App() {
   return (
     <main className="root">
-      <div className="swipe-deck-wrapper-v2">
-        {Array.from({ length: 6 }, (_, k) => {
-          return (
-            <div
-              key={k}
-              style={{ "--index": k + 1 } as CSSProperties}
-              className="swipe-deck-card-v2"
-            >
-              <span></span>
-            </div>
-          );
-        })}
-      </div>
-
       <div className="swipe-deck-wrapper">
-        {Array.from({ length: 20 }, (_, k) => {
+        {Array.from({ length: 6 }, (_, k) => {
           return (
             <div
               key={k}
